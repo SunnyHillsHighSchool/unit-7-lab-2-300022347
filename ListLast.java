f889e3e37f454148b8ee457602332e53c0d2dd54
Kyle Kim <300022347@fjuhsd.org>
2021-02-04 05:02:44 +0000

    Completed unit 7 lab 2.

--- ListLast.java
+++ ListLast.java
@@ -3,13 +3,33 @@
 //Name -
 //Date -
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 
 public class ListLast
 {
+  
 	public static boolean go( List<Integer> ray)
 	{
-		return true;
+    int lastItem = ray.get(ray.size()-1);
+    boolean outcome = false;
+    for(int i=ray.size()-2; i>=0; i--)
+    {
+      if (lastItem == ray.get(i))
+      {
+        outcome = true;
+        break;
+      }
+
+      else
+      {
+        outcome = false;
+      }
+
+      
+    }
+    System.out.println(lastItem);
+		return outcome;
 	}
 }
--- Main.java
+++ Main.java
@@ -4,10 +4,14 @@
 //Date -
 
 //add imports as needed
+import java.util.Arrays;
 
 public class Main
 {
  public static void main( String args[] )
  {
+   ListLast l = new ListLast();
+   System.out.println( l.go( Arrays.asList(-99,1,2,3,4,5,6,7,8,9,10,5) ) );
+
  }
 }
